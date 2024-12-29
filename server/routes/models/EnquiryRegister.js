5a456006bf397579b2c025cde7b1c397a2e1cbde
sajjansonenterprises <sajjanson.test@gmail.com>
2024-12-29 12:31:15 +0530

    Update EnquiryRegister.js

--- server/routes/models/EnquiryRegister.js
+++ server/routes/models/EnquiryRegister.js
@@ -1,1 +1,30 @@
 
+const mongoose = require('mongoose');
+
+// Define the schema for the MillCard
+const millCardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, // Title is required
+  },
+  subject: {
+    type: String,
+    required: true, // Content is required
+  },
+  email: {
+    type: String,
+    required: false, // Thumbnail URL is optional (it may not always be provided)
+  },
+    message: {
+    type: String,
+    required: false, // Thumbnail URL is optional (it may not always be provided)
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now, // Store the date the document was created
+  },
+});
+
+// Create and export the model
+const EnquiryRegister = mongoose.model('MillCard', millCardSchema);
+module.exports = EnquiryRegister;
